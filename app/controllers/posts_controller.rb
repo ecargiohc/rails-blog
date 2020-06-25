c8358733c2208588f48d21b1386cf42d24a8d87d
github username <therealgracechoi@gmail.com>
2020-06-25 10:09:04 -0700

    new.html.erb created for new post form and create added to posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,29 @@ class PostsController < ApplicationController
     end
 
     def new
+        @post = Post.new
     end
 
     def create
+        # need to pass in the parameters that we used to create posts
+        # we'll use the *private* method below  
+        @post = Post.new(post_params)
+
+        # check to see if post was saved
+        # if so, we'll redirect to post itself
+        # else, render the new template again
+        if @post.save
+            redirect_to @post
+        else 
+            render "new"
+        end
     end
+
+    private
+
+    def post_params
+        # if not initialized here, they won't be submitted to server
+        params.require(:post).permit(:title, :content)
+    end
+
 end
